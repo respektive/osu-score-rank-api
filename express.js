9fb1c97f79edbb9f31b2cd04e17d17755f7c4699
respektive <liambzw@gmail.com>
2023-09-04 21:28:19 +0200

    fix some formatting

--- express.js
+++ express.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const morgan = require('morgan')
+const express = require("express");
+const morgan = require("morgan");
 const Redis = require("ioredis");
 const redisClient = new Redis();
 const config = require("./config");
-const mariadb = require('mariadb');
+const mariadb = require("mariadb");
 const pool = mariadb.createPool({
-     host: config.db.host, 
-     user: config.db.user, 
-     password: config.db.pw,
-     database: config.db.db,
-     connectionLimit: 5
+    host: config.db.host,
+    user: config.db.user,
+    password: config.db.pw,
+    database: config.db.db,
+    connectionLimit: 5,
 });
 
 const MODES = {
-    "osu": 0,
-    "taiko": 1,
-    "fruits": 2,
-    "mania": 3,
-}
+    osu: 0,
+    taiko: 1,
+    fruits: 2,
+    mania: 3,
+};
 
 const api = express();
 const port = config.api.port;
@@ -48,7 +48,6 @@ function parseMode(mode, m) {
             default:
                 resolveMode = "osu";
                 break;
-
         }
     }
     return resolveMode;
@@ -56,7 +55,7 @@ function parseMode(mode, m) {
 
 function isEmpty(object) {
     for (const property in object) {
-      return false;
+        return false;
     }
     return true;
 }
@@ -66,39 +65,46 @@ async function getPeakRank(user_id, mode) {
     let rows;
     try {
         conn = await pool.getConnection();
-        rows = await conn.query("SELECT * FROM osu_score_rank_highest WHERE user_id = ? AND mode = ?", [user_id, MODES[mode]]);
+        rows = await conn.query(
+            "SELECT * FROM osu_score_rank_highest WHERE user_id = ? AND mode = ?",
+            [user_id, MODES[mode]]
+        );
     } finally {
         if (conn) conn.end();
     }
-    let rank_highest = {rank: rows[0]?.rank ?? 0, updated_at: rows[0]?.updated_at ?? null}
+    let rank_highest = { rank: rows[0]?.rank ?? 0, updated_at: rows[0]?.updated_at ?? null };
     return rank_highest;
 }
 
 async function main() {
-
     api.listen(port, () => {
         console.log(`api listening on port ${port}`);
     });
 
-    api.use(morgan('dev'))
-    api.use(require('express-status-monitor')());
+    api.use(morgan("dev"));
+    api.use(require("express-status-monitor")());
 
-    api.get('/rank/*', async (req, res) => {
+    api.get("/rank/*", async (req, res) => {
         let mode = await parseMode(req.query.mode, req.query.m);
 
         let rank = req.path.split("/").pop();
-        let rank_user = await redisClient.zrevrange(`score_${mode}`, rank - 1 , rank - 1 , "WITHSCORES");
+        let rank_user = await redisClient.zrevrange(
+            `score_${mode}`,
+            rank - 1,
+            rank - 1,
+            "WITHSCORES"
+        );
         let data = {};
 
-        for(let i = 0; i < rank_user.length; i+=2) {
-            data["rank"] = parseInt(rank)
-            data["user_id"] = parseInt(rank_user[i])
+        for (let i = 0; i < rank_user.length; i += 2) {
+            data["rank"] = parseInt(rank);
+            data["user_id"] = parseInt(rank_user[i]);
             data["username"] = await redisClient.get(`user_${rank_user[i]}`);
-            data["score"] = parseInt(rank_user[i+1])
+            data["score"] = parseInt(rank_user[i + 1]);
             data["rank_highest"] = await getPeakRank(rank_user[i], mode);
         }
 
-        if(isEmpty(data)) {
+        if (isEmpty(data)) {
             res.status(200);
             res.json([{ rank: 0, user_id: 0, username: 0, score: 0 }]);
         } else {
@@ -107,83 +113,89 @@ async function main() {
         }
     });
 
-    api.get('/u/:users', async (req, res) => {
+    api.get("/u/:users", async (req, res) => {
         let mode = parseMode(req.query.mode, req.query.m);
-        let users = req.params.users.split(',');
-    
+        let users = req.params.users.split(",");
+
         if (["username", "user_id"].includes(req.query.s) == -1 || req.query.s == undefined) {
-            req.query.s = "user_id"
+            req.query.s = "user_id";
         }
-    
+
         let results = [];
-    
+
         if (users.length > 100) {
             res.status(400);
-            res.json({ error: 'Too many users. Max limit is 100.' });
+            res.json({ error: "Too many users. Max limit is 100." });
             return;
         }
-    
-        await Promise.all(users.map(async (user) => {
-            let user_id;
-            if (req.query.s == "username") {
-                user_id = await redisClient.get(`user_${user}`);
-            } else {
-                user_id = user;
-            }
 
-            let rank_highest = await getPeakRank(user_id, mode);
-            
-            let [score, rank, usernameValue] = await Promise.all([
-                redisClient.zscore(`score_${mode}`, user_id),
-                redisClient.zrevrank(`score_${mode}`, user_id),
-                redisClient.get(`user_${user_id}`)
-            ]);
-            let data = {
-                rank: rank + 1,
-                user_id: parseInt(user_id),
-                username: usernameValue,
-                score: parseInt(score),
-                rank_highest: rank_highest ?? null,
-            }
-            if (isNaN(data.score)) {
-                results.push({ rank: 0, user_id: 0, username: 0, score: 0 });
-            } else {
+        await Promise.all(
+            users.map(async (user) => {
+                let user_id;
+                if (req.query.s == "username") {
+                    user_id = await redisClient.get(`user_${user}`);
+                } else {
+                    user_id = user;
+                }
+
+                let rank_highest = await getPeakRank(user_id, mode);
+
+                let [score, rank, usernameValue] = await Promise.all([
+                    redisClient.zscore(`score_${mode}`, user_id),
+                    redisClient.zrevrank(`score_${mode}`, user_id),
+                    redisClient.get(`user_${user_id}`),
+                ]);
+                let data = {
+                    rank: rank ? rank + 1 : 0,
+                    user_id: parseInt(user_id) || 0,
+                    username: usernameValue || 0,
+                    score: parseInt(score) || 0,
+                    rank_highest: rank_highest ?? null,
+                };
                 results.push(data);
-            }
-        }));
-    
+            })
+        );
+
         res.status(200);
         res.json(results);
-    });    
+    });
 
-    api.get('/rankings', async (req, res) => {
-
+    api.get("/rankings", async (req, res) => {
         let mode = await parseMode(req.query.mode, req.query.m);
 
-        if (req.query.page > 200 || req.query.page < 1 || req.query.page == undefined || isNaN(req.query.page)) {
+        if (
+            req.query.page > 200 ||
+            req.query.page < 1 ||
+            req.query.page == undefined ||
+            isNaN(req.query.page)
+        ) {
             req.query.page = 1;
         }
-        
+
         let start_rank = (req.query.page - 1) * 50;
-        let rankings = await redisClient.zrevrange(`score_${mode}`, start_rank, start_rank + 49, "WITHSCORES");
+        let rankings = await redisClient.zrevrange(
+            `score_${mode}`,
+            start_rank,
+            start_rank + 49,
+            "WITHSCORES"
+        );
 
         let lb = {};
         let r = 0;
-    
-        for(let i = 0; i < rankings.length; i+=2) {
+
+        for (let i = 0; i < rankings.length; i += 2) {
             lb[r] = {};
-            lb[r]["rank"] = await redisClient.zrevrank(`score_${mode}`, rankings[i]) + 1;
+            lb[r]["rank"] = (await redisClient.zrevrank(`score_${mode}`, rankings[i])) + 1;
             lb[r]["user_id"] = parseInt(rankings[i]);
             lb[r]["username"] = await redisClient.get(`user_${rankings[i]}`);
-            lb[r]["score"] = parseInt(rankings[i+1]);
+            lb[r]["score"] = parseInt(rankings[i + 1]);
             lb[r]["rank_highest"] = await getPeakRank(rankings[i], mode);
-            r++
+            r++;
         }
 
         res.status(200);
         res.json(lb);
     });
-
 }
 
 main();
--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const axios = require("axios");
 const Redis = require("ioredis");
 const redisClient = new Redis();
 const config = require("./config");
-const mariadb = require('mariadb');
+const mariadb = require("mariadb");
 const pool = mariadb.createPool({
-     host: config.db.host, 
-     user: config.db.user, 
-     password: config.db.pw,
-     database: config.db.db,
-     connectionLimit: 5
+    host: config.db.host,
+    user: config.db.user,
+    password: config.db.pw,
+    database: config.db.db,
+    connectionLimit: 5,
 });
 
 const MODES = {
-    "osu": 0,
-    "taiko": 1,
-    "fruits": 2,
-    "mania": 3,
-}
+    osu: 0,
+    taiko: 1,
+    fruits: 2,
+    mania: 3,
+};
 
 let token;
 let entries = 0;
@@ -24,18 +24,21 @@ let refresh = 0;
 let user_ids = [];
 let retries = {
     osu: {
-        score: 0
-    }, mania: {
-        score: 0
-    }, taiko: {
-        score: 0
-    }, fruits: {
-        score: 0
-    }
-}
+        score: 0,
+    },
+    mania: {
+        score: 0,
+    },
+    taiko: {
+        score: 0,
+    },
+    fruits: {
+        score: 0,
+    },
+};
 let done = true;
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function refreshToken() {
     return new Promise((resolve, reject) => {
@@ -43,21 +46,23 @@ async function refreshToken() {
             url: "https://osu.ppy.sh/oauth/token",
             method: "post",
             headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
+                Accept: "application/json",
+                "Content-Type": "application/json",
             },
             data: {
-                "grant_type": "client_credentials",
-                "client_id": config.osu.id,
-                "client_secret": config.osu.secret,
-                "scope": "public"
-            }
-        }).then(data => {
-            refresh = Date.now() + (data.data.expires_in * 1000);
-            resolve('Bearer ' + data.data.access_token);
-        }).catch(err => {
-            reject(err);
-        });
+                grant_type: "client_credentials",
+                client_id: config.osu.id,
+                client_secret: config.osu.secret,
+                scope: "public",
+            },
+        })
+            .then((data) => {
+                refresh = Date.now() + data.data.expires_in * 1000;
+                resolve("Bearer " + data.data.access_token);
+            })
+            .catch((err) => {
+                reject(err);
+            });
     });
 }
 
@@ -67,84 +72,104 @@ async function fullRankingsUpdate(mode, type, cursor) {
         token = await refreshToken();
     }
 
-    let osuAPI = axios.create({ baseURL: 'https://osu.ppy.sh/api/v2', headers: { 'Authorization': token }, json: true });
+    let osuAPI = axios.create({
+        baseURL: "https://osu.ppy.sh/api/v2",
+        headers: { Authorization: token },
+        json: true,
+    });
 
-    osuAPI.get('/rankings/' + mode + '/' + type, { data: { cursor: { page: cursor } } }).then(async res => {
-        let i = 0;
+    osuAPI
+        .get("/rankings/" + mode + "/" + type, { data: { cursor: { page: cursor } } })
+        .then(async (res) => {
+            let i = 0;
 
-        console.log("Adding " + res.data.ranking.length + " Entries to the db");
+            console.log("Adding " + res.data.ranking.length + " Entries to the db");
 
-        await res.data.ranking.forEach(async elem => {
-            i++
-            entries++
-            user_ids.push(elem.user.id);
+            await res.data.ranking.forEach(async (elem) => {
+                i++;
+                entries++;
+                user_ids.push(elem.user.id);
 
-            await redisClient.zadd(`score_${mode}`, elem.ranked_score, elem.user.id);
-            await redisClient.set(`user_${elem.user.id}`, elem.user.username);
-            await redisClient.set(`user_${elem.user.username}`, elem.user.id);
-            try {
-                conn = await pool.getConnection();
-                const rows = await conn.query("SELECT rank FROM osu_score_rank_highest WHERE user_id = ? AND mode = ?", [elem.user.id, MODES[mode]]);
+                await redisClient.zadd(`score_${mode}`, elem.ranked_score, elem.user.id);
+                await redisClient.set(`user_${elem.user.id}`, elem.user.username);
+                await redisClient.set(`user_${elem.user.username}`, elem.user.id);
+                try {
+                    conn = await pool.getConnection();
+                    const rows = await conn.query(
+                        "SELECT rank FROM osu_score_rank_highest WHERE user_id = ? AND mode = ?",
+                        [elem.user.id, MODES[mode]]
+                    );
 
-                const rank = await redisClient.zrevrank(`score_${mode}`, elem.user.id);
-                if (!rows[0] || rank+1 > rows[0].rank ) {
-                    const res = await conn.query("INSERT INTO osu_score_rank_highest (user_id, mode, rank) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE rank=?", [elem.user.id, MODES[mode], rank+1, rank+1])
-                    console.log("Added new highest rank", rank+1, "for user", elem.user.username, "and mode", mode);
+                    const rank = await redisClient.zrevrank(`score_${mode}`, elem.user.id);
+                    if (!rows[0] || rank + 1 > rows[0].rank) {
+                        const res = await conn.query(
+                            "INSERT INTO osu_score_rank_highest (user_id, mode, rank) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE rank=?",
+                            [elem.user.id, MODES[mode], rank + 1, rank + 1]
+                        );
+                        console.log(
+                            "Added new highest rank",
+                            rank + 1,
+                            "for user",
+                            elem.user.username,
+                            "and mode",
+                            mode
+                        );
+                    }
+                } finally {
+                    if (conn) conn.end();
                 }
-            } finally {
-                if (conn) conn.end();
+            });
+
+            if (res.data.cursor != null) {
+                cursor = res.data.cursor.page;
+                await sleep(1000);
+                fullRankingsUpdate(mode, type, cursor);
+                retries[mode][type] = 0;
+                console.log("Added a total of " + entries + " to the db score_" + mode);
+            } else {
+                // Remove restricted and otherwise deleted users from the api.
+                const redis_users = await redisClient.zrange(`score_${mode}`, 0, -1);
+                for (id of redis_users) {
+                    if (!user_ids.includes(Number(id))) {
+                        await redisClient.zrem(`score_${mode}`, id);
+                        console.log("Removed user_id:", id);
+                    }
+                }
+                console.log("Finished iterating for a total of " + entries + " Entries!");
+                entries = 0;
+                user_ids = [];
+                done = true;
+            }
+        })
+        .catch(async (err) => {
+            if (retries[mode][type] < 4) {
+                console.log(err);
+                console.log("Retry: " + retries[mode][type]);
+                retries[mode][type]++;
+                await sleep(1000 * (retries[mode][type] * 10));
+                fullRankingsUpdate(mode, type, cursor);
+            } else {
+                console.log("Max retries reached, giving up.");
+                retries[mode][type] = 0;
+                done = true;
             }
         });
-
-        if (res.data.cursor != null) {
-            cursor = res.data.cursor.page;
-            await sleep(1000);
-            fullRankingsUpdate(mode, type, cursor);
-            retries[mode][type] = 0;
-            console.log("Added a total of " + entries + " to the db score_" + mode);
-        } else {
-            // Remove restricted and otherwise deleted users from the api.
-            const redis_users = await redisClient.zrange(`score_${mode}`, 0, -1);
-            for (id of redis_users) {
-                if (!user_ids.includes(Number(id))) {
-                    await redisClient.zrem(`score_${mode}`, id);
-                    console.log("Removed user_id:", id);
-                }
-            }
-            console.log("Finished iterating for a total of " + entries + " Entries!");
-            entries = 0;
-            user_ids = [];
-            done = true;
-        }
-    }).catch(async err => {
-        if (retries[mode][type] < 4) {
-            console.log(err);
-            console.log("Retry: " + retries[mode][type]);
-            retries[mode][type]++
-            await sleep(1000 * (retries[mode][type] * 10));
-            fullRankingsUpdate(mode, type, cursor);
-        } else {
-            console.log("Max retries reached, giving up.");
-            retries[mode][type] = 0;
-            done = true;
-        }
-    });
 }
 
 let m = -1;
 
 function updateAll() {
     if (!done) {
-        console.log("fetching not done yet, waiting for next interval.")
-        return
+        console.log("fetching not done yet, waiting for next interval.");
+        return;
     }
 
-    m++
-    if(m > 3){
-        m = 0
+    m++;
+    if (m > 3) {
+        m = 0;
     }
-    
-    switch(m) {
+
+    switch (m) {
         default:
         case 0:
             fullRankingsUpdate("osu", "score", 1);
@@ -160,7 +185,7 @@ function updateAll() {
             fullRankingsUpdate("fruits", "score", 1);
             done = false;
             console.log("Starting fetch for osu!catch");
-            break;        
+            break;
         case 3:
             fullRankingsUpdate("mania", "score", 1);
             done = false;
