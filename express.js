be64ed1fc2c4263c53e8aef75104ed3dbf6d88c3
hi <haxordp@gmail.com>
2026-06-24 18:29:47 +0200

    add prettier and unify formatting

--- express.js
+++ express.js
@@ -3,224 +3,216 @@ const Redis = require("ioredis");
 const redisClient = new Redis();
 const config = require("./config");
 const { metricsServer, observeDbQueryDuration, observeRequestDuration } = require("./metrics");
-const { getPeakRank, getRankHistory, isStringInteger, isEmpty, resolveModeName, guessOriginFromRequestHeaders, MAX_USERS_PER_REQUEST } = require("./helpers");
+const {
+	getPeakRank,
+	getRankHistory,
+	isStringInteger,
+	isEmpty,
+	resolveModeName,
+	guessOriginFromRequestHeaders,
+	MAX_USERS_PER_REQUEST
+} = require("./helpers");
 const responseTime = require("response-time");
 const mariadb = require("mariadb");
 const pool = mariadb.createPool({
-    host: config.db.host,
-    user: config.db.user,
-    password: config.db.pw,
-    database: config.db.db,
-    connectionLimit: 5,
+	host: config.db.host,
+	user: config.db.user,
+	password: config.db.pw,
+	database: config.db.db,
+	connectionLimit: 5
 });
 
 const api = express();
 const port = config.api.port;
 
 async function getUserAtRank(rank, mode) {
-    const rankUser = await redisClient.zrevrange(
-        `score_${mode}`,
-        rank - 1,
-        rank - 1,
-        "WITHSCORES"
-    );
+	const rankUser = await redisClient.zrevrange(`score_${mode}`, rank - 1, rank - 1, "WITHSCORES");
 
-    if (!rankUser?.length) return {};
+	if (!rankUser?.length) return {};
 
-    const userId = rankUser[0];
-    const rankHighestMap = await getPeakRank([userId], mode);
-    const rankHistoryMap = await getRankHistory([userId], mode);
+	const userId = rankUser[0];
+	const rankHighestMap = await getPeakRank([userId], mode);
+	const rankHistoryMap = await getRankHistory([userId], mode);
 
-    return {
-        rank: parseInt(rank),
-        user_id: parseInt(userId),
-        username: await redisClient.hget("user_id_to_username", userId),
-        score: parseInt(rankUser[1]),
-        rank_highest: rankHighestMap[userId] ?? null,
-        rank_history: rankHistoryMap[userId] ?? null,
-    };
+	return {
+		rank: parseInt(rank),
+		user_id: parseInt(userId),
+		username: await redisClient.hget("user_id_to_username", userId),
+		score: parseInt(rankUser[1]),
+		rank_highest: rankHighestMap[userId] ?? null,
+		rank_history: rankHistoryMap[userId] ?? null
+	};
 }
 
 async function main() {
-    api.listen(port, () => console.log(`api listening on port ${port}`));
+	api.listen(port, () => console.log(`api listening on port ${port}`));
 
-    api.use(require("express-status-monitor")());
+	api.use(require("express-status-monitor")());
 
-    api.use(
-        responseTime((req, res, response_time) => {
-            if (!req?.route?.path) return;
+	api.use(
+		responseTime((req, res, response_time) => {
+			if (!req?.route?.path) return;
 
-            observeRequestDuration(
-                response_time / 1000,
-                req.method,
-                req.route.path,
-                res.statusCode,
-                guessOriginFromRequestHeaders(req),
-                resolveModeName(req.query.mode, req.query.m)
-            );
-        })
-    );
+			observeRequestDuration(
+				response_time / 1000,
+				req.method,
+				req.route.path,
+				res.statusCode,
+				guessOriginFromRequestHeaders(req),
+				resolveModeName(req.query.mode, req.query.m)
+			);
+		})
+	);
 
-    api.get("/rank/*", async (req, res) => {
-        const mode = resolveModeName(req.query.mode, req.query.m);
-        const rank = req.path.split("/").pop();
+	api.get("/rank/*", async (req, res) => {
+		const mode = resolveModeName(req.query.mode, req.query.m);
+		const rank = req.path.split("/").pop();
 
-        if (!isStringInteger(rank)) {
-            res.status(400);
-            res.json({ error: "Invalid Rank" });
-            return;
-        }
+		if (!isStringInteger(rank)) {
+			res.status(400);
+			res.json({ error: "Invalid Rank" });
+			return;
+		}
 
-        const data = await getUserAtRank(rank, mode);
+		const data = await getUserAtRank(rank, mode);
 
-        if (isEmpty(data)) {
-            res.status(200);
-            res.json([{ rank: 0, user_id: 0, username: 0, score: 0 }]);
-        } else {
-            res.status(200);
-            res.json([data]);
-        }
-    });
+		if (isEmpty(data)) {
+			res.status(200);
+			res.json([{ rank: 0, user_id: 0, username: 0, score: 0 }]);
+		} else {
+			res.status(200);
+			res.json([data]);
+		}
+	});
 
-    api.get("/u/:users", async (req, res) => {
-        const mode = resolveModeName(req.query.mode, req.query.m);
-        const scores = req.query.score?.split(",") ?? [];
-        const users = [...new Set(req.params.users.split(","))];
-        if (users.length > MAX_USERS_PER_REQUEST) {
-            res.status(400);
-            res.json({ error: `Too many users. The limit is ${MAX_USERS_PER_REQUEST}.` });
-            return;
-        }
+	api.get("/u/:users", async (req, res) => {
+		const mode = resolveModeName(req.query.mode, req.query.m);
+		const scores = req.query.score?.split(",") ?? [];
+		const users = [...new Set(req.params.users.split(","))];
+		if (users.length > MAX_USERS_PER_REQUEST) {
+			res.status(400);
+			res.json({ error: `Too many users. The limit is ${MAX_USERS_PER_REQUEST}.` });
+			return;
+		}
 
-        if (!["username", "user_id"].includes(req.query.s)) req.query.s = "user_id";
+		if (!["username", "user_id"].includes(req.query.s)) req.query.s = "user_id";
 
-        const userIds = [];
-        for (const user of users) {
-            const userId =
-                req.query.s == "username"
-                    ? await redisClient.hget("username_to_user_id", user)
-                    : user;
+		const userIds = [];
+		for (const user of users) {
+			const userId = req.query.s == "username" ? await redisClient.hget("username_to_user_id", user) : user;
 
-            if (!isStringInteger(userId)) {
-                res.status(400);
-                res.json({ error: "Invalid User" });
-                return;
-            }
+			if (!isStringInteger(userId)) {
+				res.status(400);
+				res.json({ error: "Invalid User" });
+				return;
+			}
 
-            userIds.push(userId);
-        }
+			userIds.push(userId);
+		}
 
-        const rankHighestMap = await getPeakRank(userIds, mode);
-        const rankHistoryMap = await getRankHistory(userIds, mode);
+		const rankHighestMap = await getPeakRank(userIds, mode);
+		const rankHistoryMap = await getRankHistory(userIds, mode);
 
-        const results = [];
-        for (const [index, userId] of userIds.entries()) {
-            const rankHighest = rankHighestMap[userId] ?? null;
-            const rankHistory = rankHistoryMap[userId] ?? null;
-            const username = await redisClient.hget("user_id_to_username", userId);
+		const results = [];
+		for (const [index, userId] of userIds.entries()) {
+			const rankHighest = rankHighestMap[userId] ?? null;
+			const rankHistory = rankHistoryMap[userId] ?? null;
+			const username = await redisClient.hget("user_id_to_username", userId);
 
-            let score, rank;
-            if (scores[index] != undefined) {
-                if (!isStringInteger(scores[index])) {
-                    res.status(400);
-                    res.json({ error: "Invalid Score" });
-                    return;
-                }
+			let score, rank;
+			if (scores[index] != undefined) {
+				if (!isStringInteger(scores[index])) {
+					res.status(400);
+					res.json({ error: "Invalid Score" });
+					return;
+				}
 
-                score = scores[index];
-                const belowRankUser = await redisClient.zrange(`score_${mode}`, score, 0, 'BYSCORE', 'REV', 'LIMIT', 0, 1);
-                const belowRank = belowRankUser.length == 0 ? 10000 : await redisClient.zrevrank(`score_${mode}`, belowRankUser);
-                rank = belowRank;
-            } else {
-                score = await redisClient.zscore(`score_${mode}`, userId);
-                rank = await redisClient.zrevrank(`score_${mode}`, userId);
-            }
+				score = scores[index];
+				const belowRankUser = await redisClient.zrange(`score_${mode}`, score, 0, "BYSCORE", "REV", "LIMIT", 0, 1);
+				const belowRank =
+					belowRankUser.length == 0 ? 10000 : await redisClient.zrevrank(`score_${mode}`, belowRankUser);
+				rank = belowRank;
+			} else {
+				score = await redisClient.zscore(`score_${mode}`, userId);
+				rank = await redisClient.zrevrank(`score_${mode}`, userId);
+			}
 
-            const nextRaw = rank == 0 ? null : await getUserAtRank(rank, mode);
-            const next = isEmpty(nextRaw) ? null : {
-                username: nextRaw.username,
-                user_id: nextRaw.user_id,
-                score: nextRaw.score,
-            };
+			const nextRaw = rank == 0 ? null : await getUserAtRank(rank, mode);
+			const next = isEmpty(nextRaw)
+				? null
+				: {
+						username: nextRaw.username,
+						user_id: nextRaw.user_id,
+						score: nextRaw.score
+					};
 
-            let prevRaw;
-            for (let i = 0; i <= 1; i++) {
-                prevRaw = await getUserAtRank(rank + 1 + i, mode);
-                if (prevRaw.user_id != parseInt(userId)) break;
-            }
+			let prevRaw;
+			for (let i = 0; i <= 1; i++) {
+				prevRaw = await getUserAtRank(rank + 1 + i, mode);
+				if (prevRaw.user_id != parseInt(userId)) break;
+			}
 
-            const prev = isEmpty(prevRaw) ? null : {
-                username: prevRaw.username,
-                user_id: prevRaw.user_id,
-                score: prevRaw.score,
-            };
+			const prev = isEmpty(prevRaw)
+				? null
+				: {
+						username: prevRaw.username,
+						user_id: prevRaw.user_id,
+						score: prevRaw.score
+					};
 
-            const data = {
-                rank: rank == null ? 0 : rank + 1,
-                user_id: parseInt(userId) || 0,
-                username: username || 0,
-                score: parseInt(score) || 0,
-                rank_highest: rankHighest,
-                rank_history: rankHistory,
-                prev,
-                next,
-            };
-            results.push(data);
-        }
+			const data = {
+				rank: rank == null ? 0 : rank + 1,
+				user_id: parseInt(userId) || 0,
+				username: username || 0,
+				score: parseInt(score) || 0,
+				rank_highest: rankHighest,
+				rank_history: rankHistory,
+				prev,
+				next
+			};
+			results.push(data);
+		}
 
-        res.status(200);
-        res.json(results);
-    });
+		res.status(200);
+		res.json(results);
+	});
 
-    api.get("/rankings", async (req, res) => {
-        const mode = resolveModeName(req.query.mode, req.query.m);
+	api.get("/rankings", async (req, res) => {
+		const mode = resolveModeName(req.query.mode, req.query.m);
 
-        if (
-            req.query.page > 200 ||
-            req.query.page < 1 ||
-            req.query.page == undefined ||
-            isNaN(req.query.page)
-        ) {
-            req.query.page = 1;
-        }
+		if (req.query.page > 200 || req.query.page < 1 || req.query.page == undefined || isNaN(req.query.page))
+			req.query.page = 1;
 
-        const startRank = (req.query.page - 1) * 50;
-        const rankings = await redisClient.zrevrange(
-            `score_${mode}`,
-            startRank,
-            startRank + 49,
-            "WITHSCORES"
-        );
+		const startRank = (req.query.page - 1) * 50;
+		const rankings = await redisClient.zrevrange(`score_${mode}`, startRank, startRank + 49, "WITHSCORES");
 
-        const lb = {};
-        const userIds = [];
-        
-        for (let i = 0; i < rankings.length; i += 2) userIds.push(rankings[i]);
-        
-        const rankHighestMap = await getPeakRank(userIds, mode);
-        const rankHistoryMap = await getRankHistory(userIds, mode);
-        
-        let r = 0;
-        for (let i = 0; i < rankings.length; i += 2) {
-            const userId = rankings[i];
-            lb[r] = {};
-            lb[r]["rank"] = (await redisClient.zrevrank(`score_${mode}`, userId)) + 1;
-            lb[r]["user_id"] = parseInt(userId);
-            lb[r]["username"] = await redisClient.hget("user_id_to_username", userId);
-            lb[r]["score"] = parseInt(rankings[i + 1]);
-            lb[r]["rank_highest"] = rankHighestMap[userId] ?? null;
-            lb[r]["rank_history"] = rankHistoryMap[userId] ?? null;
-            r++;
-        }
+		const lb = {};
+		const userIds = [];
+		for (let i = 0; i < rankings.length; i += 2) userIds.push(rankings[i]);
 
-        res.status(200);
-        res.json(lb);
-    });
+		const rankHighestMap = await getPeakRank(userIds, mode);
+		const rankHistoryMap = await getRankHistory(userIds, mode);
+
+		let r = 0;
+		for (let i = 0; i < rankings.length; i += 2) {
+			const userId = rankings[i];
+			lb[r] = {};
+			lb[r]["rank"] = (await redisClient.zrevrank(`score_${mode}`, userId)) + 1;
+			lb[r]["user_id"] = parseInt(userId);
+			lb[r]["username"] = await redisClient.hget("user_id_to_username", userId);
+			lb[r]["score"] = parseInt(rankings[i + 1]);
+			lb[r]["rank_highest"] = rankHighestMap[userId] ?? null;
+			lb[r]["rank_history"] = rankHistoryMap[userId] ?? null;
+			r++;
+		}
+
+		res.status(200);
+		res.json(lb);
+	});
 }
 
 main();
 
-if (config.metrics.port > 0)
-    metricsServer(config.metrics.port);
+if (config.metrics.port > 0) metricsServer(config.metrics.port);
 
 require("./fetcher");
--- update_rank_history.js
+++ update_rank_history.js
@@ -4,64 +4,64 @@ const config = require("./config");
 const { getUserRankHistories, MS_IN_DAY, MODE_NAMES } = require("./helpers");
 const mariadb = require("mariadb");
 const pool = mariadb.createPool({
-    host: config.db.host,
-    user: config.db.user,
-    password: config.db.pw,
-    database: config.db.db,
-    connectionLimit: 15,
-})
+	host: config.db.host,
+	user: config.db.user,
+	password: config.db.pw,
+	database: config.db.db,
+	connectionLimit: 15
+});
 
 async function updateRankHistory() {
-    const today = new Date().setHours(0, 0, 0, 0);
-    let conn;
-    try {
-        conn = await pool.getConnection();
-        for (let i = 0; i < MODE_NAMES.length; i++) {
-            const users = await redisClient.zrevrange(`score_${MODE_NAMES[i]}`, 0, -1);
-            const rows = await getUserRankHistories(users, i);
-            for (const [index, user_id] of users.entries()) {
-                const row = rows[user_id];
+	const today = new Date().setHours(0, 0, 0, 0);
+	let conn;
+	try {
+		conn = await pool.getConnection();
+		for (let i = 0; i < MODE_NAMES.length; i++) {
+			const users = await redisClient.zrevrange(`score_${MODE_NAMES[i]}`, 0, -1);
+			const rows = await getUserRankHistories(users, i);
+			for (const [index, user_id] of users.entries()) {
+				const row = rows[user_id];
 
-                let rank_history;
-                if (!row?.latest_rank_date) {
-                    rank_history = [];
-                } else {
-                    const days_since_last_update = Math.floor(
-                        (today - new Date(Date.parse(row.latest_rank_date)).setHours(0, 0, 0, 0)) / MS_IN_DAY,
-                    );
+				let rank_history;
+				if (!row?.latest_rank_date) {
+					rank_history = [];
+				} else {
+					const days_since_last_update = Math.floor(
+						(today - new Date(Date.parse(row.latest_rank_date)).setHours(0, 0, 0, 0)) / MS_IN_DAY
+					);
 
-                    if (days_since_last_update >= 90) {
-                        // if the last update was over 90 days ago we can just reset the rank history
-                        rank_history = [];
-                    } else if (days_since_last_update < 1) {
-                        // this should never happen, but doesn't hurt to have as a safety guard i guess?
-                        continue;
-                    } else {
-                        rank_history = row.rank_history;
-                        // set days without data to null
-                        for (let j = 1; j < days_since_last_update; j++) {
-                            rank_history.push(null);
-                        }
-                    }
-                }
+					if (days_since_last_update >= 90) {
+						// if the last update was over 90 days ago we can just reset the rank history
+						rank_history = [];
+					} else if (days_since_last_update < 1) {
+						// this should never happen, but doesn't hurt to have as a safety guard i guess?
+						continue;
+					} else {
+						rank_history = row.rank_history;
+						// set days without data to null
+						for (let j = 1; j < days_since_last_update; j++) {
+							rank_history.push(null);
+						}
+					}
+				}
 
-                rank_history.push(index + 1);
-                // we only wanna store the last 90 days
-                while (rank_history.length > 90) rank_history.shift();
+				rank_history.push(index + 1);
+				// we only wanna store the last 90 days
+				while (rank_history.length > 90) rank_history.shift();
 
-                const res = await conn.query(
-                    "INSERT INTO osu_score_rank_history (user_id, mode, rank_history) VALUES (?, ?, json_compact(?)) ON DUPLICATE KEY UPDATE rank_history=json_compact(?)",
-                    [user_id, i, JSON.stringify(rank_history), JSON.stringify(rank_history)],
-                );
-            }
-        }
-    } finally {
-        if (conn) await conn.release();
-    }
+				const res = await conn.query(
+					"INSERT INTO osu_score_rank_history (user_id, mode, rank_history) VALUES (?, ?, json_compact(?)) ON DUPLICATE KEY UPDATE rank_history=json_compact(?)",
+					[user_id, i, JSON.stringify(rank_history), JSON.stringify(rank_history)]
+				);
+			}
+		}
+	} finally {
+		if (conn) await conn.release();
+	}
 }
 
 (async () => {
-    await updateRankHistory();
-    console.log("done updating rank history");
-    process.exit(0);
+	await updateRankHistory();
+	console.log("done updating rank history");
+	process.exit(0);
 })();
